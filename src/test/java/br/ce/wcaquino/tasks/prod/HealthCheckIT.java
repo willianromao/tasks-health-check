7c07a84d2de259a13f09e648daa12aa16b56298e
WillianRomao <willian.romao@outlook.com.br>
2020-08-09 11:00:05 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.tasks.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealthCheckIT {
+
+	@Test
+	public void healthCheck() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://52.250.57.59:4444/wd/hub"), cap);
+		try {
+			driver.navigate().to("http://187.121.80.36:8002/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+	}
+}
